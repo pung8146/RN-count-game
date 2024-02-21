917787d25109ac0e351d8d9a67b0ca2522d79c69
pung8146 <pung8146@naver.com>
2024-02-21 14:26:22 +0900

    Update StartGameScreen styles

---
+++ memo.md
@@ -0,0 +1,12 @@
+# boxShadow
+
+## Android
+
+> elevation 0,1,2,3... 높을 수록 그림자가 선명해진다.
+
+## IOS
+
+- shadowColor : 'black' : 원하는 그림자색
+- shadowOffset : {width: 0, height: 2} : 그림자 위치 // 0,2는 아래로 그림자가 생김
+- shadowRadius : 6 : 그림자 둥글기
+- shadowOpacity : 0.26 : 그림자 투명도
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -4,8 +4,8 @@ import PrimaryButton from "../components/PrimaryButton";
 
 const StartGameScreen = () => {
   return (
-    <View>
-      <TextInput />
+    <View style={styles.inputContainer}>
+      <TextInput style={styles.numberInput} maxLength={2} />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confrim</PrimaryButton>
     </View>
@@ -14,4 +14,31 @@ const StartGameScreen = () => {
 
 export default StartGameScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  inputContainer: {
+    // flex: 1,
+    padding: 16,
+    marginTop: 100,
+    marginHorizontal: 24,
+    backgroundColor: "#72063c",
+    // alignItems: "center",
+    // justifyContent: "center",
+    borderRadius: 8,
+    elevation: 10,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.26,
+  },
+  numberInput: {
+    width: 50,
+    height: 50,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
