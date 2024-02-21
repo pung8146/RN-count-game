f896be46a6fc55b0b08122ee227ebe21bb44bc97
pung8146 <pung8146@naver.com>
2024-02-21 13:31:17 +0900

    숫자 맞추기 앱 제작 처음 컴포넌트 파일 구성

---
+++ components/PrimaryButton.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const PrimaryButton = () => {
+  return (
+    <View>
+      <Text>PrimaryButton</Text>
+    </View>
+  );
+};
+
+export default PrimaryButton;
+
+const styles = StyleSheet.create({});
---
+++ screens/StartGameScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View, TextInput } from "react-native";
+import React from "react";
+
+const StartGameScreen = () => {
+  return (
+    <View>
+      <TextInput />
+    </View>
+  );
+};
+
+export default StartGameScreen;
+
+const styles = StyleSheet.create({});
