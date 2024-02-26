d5c8b4c065648dbcc42d771239c938e8cc04db6c
pung8146 <pung8146@naver.com>
2024-02-26 18:42:34 +0900

    게임 오버시 페이지 화면 추가 구현

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,14 +1,43 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 import React from "react";
+import Title from "../components/ui/Title";
+import { Colors } from "react-native/Libraries/NewAppScreen";
 
 const GameOverScreen = () => {
   return (
-    <View>
-      <Text>GameOverScreen</Text>
+    <View style={styles.rootContainer}>
+      <Title>GAME OVER</Title>
+      <View style={styles.imageContainer}>
+        <Image
+          style={styles.image}
+          source={require("../assets/images/success.png")}
+        />
+      </View>
+      <Text>Your phone needed X rounds to guess the number Y. </Text>
     </View>
   );
 };
 
 export default GameOverScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 24,
+  },
+  imageContainer: {
+    width: 300,
+    height: 300,
+    borderRadius: 150,
+    borderWidth: 3,
+    borderColor: Colors.primary800,
+    overflow: "hidden",
+    margin: 36,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+});
